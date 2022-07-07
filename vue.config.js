2a935709a4160ab870e01000c91477fe6a4e8daa
darl8910 <darl_8910@hotmail.com>
2022-07-07 14:38:17 -0500

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/todo-list/dist/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/challenge_vue/dist/' : '/'
 })
